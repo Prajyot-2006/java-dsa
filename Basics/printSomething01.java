9125cf15c87512e2c4876db25d52f54abd110bcf
Prajyot Jore <prajyotjore06@gmail.com>
2025-11-28 18:54:09 +0530

    Initial commit

---
+++ Basics/printSomething01.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class printSomething01 {
+    public static void main(String[] args) {
+        System.out.print("Hello this is the 1st program of JAVA DSA playlist");
+        System.out.println("Ichigo's bankai\n");
+        System.out.println("Urahara's shikai");
+        System.out.println("j");
+    }
+    
+}
