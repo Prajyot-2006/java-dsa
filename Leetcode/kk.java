ec218829ae6b2cbb9d9b587d132670d9c34df45b
Prajyot Jore <prajyotjore06@gmail.com>
2026-07-19 18:54:51 +0530

    Continue Recursion part 2

--- Leetcode/kk.java
+++
@@ -1,8 +0,0 @@
-public class kk {
-    public static void main(String[] args) {
-        int n = 129;
-        System.out.println(n%10);
-        n = n/10;
-        System.out.println(n%10);
-    }
-}
