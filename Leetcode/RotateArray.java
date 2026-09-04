8064a58c77a6630ac35554a4edb1c2f265ff8c29
Prajyot Jore <prajyotjore06@gmail.com>
2026-09-04 18:30:00 +0530

    pushed

--- Leetcode/RotateArray.java
+++ Leetcode/RotateArray.java
@@ -1,18 +1,24 @@
+
+
 import java.util.*;
 
-public class machineTest {
+public class RotateArray {  // file name 
 
+    // Function to rotate the array
     public static void rotateArray(int[] nums, int k) {
         int n = nums.length;
+
+        // Handle cases where k > n
         k = k % n;
 
         int[] rotated = new int[n];
 
-       
+        // Place elements at correct rotated position
         for (int i = 0; i < n; i++) {
             rotated[(i + k) % n] = nums[i];
         }
 
+        // Print rotated array
         for (int i = 0; i < n; i++) {
             System.out.print(rotated[i] + " ");
         }
@@ -20,6 +26,8 @@ public class machineTest {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // Read array elements
         String[] input = sc.nextLine().split(" ");
         int[] nums = new int[input.length];
 
@@ -27,9 +35,12 @@ public class machineTest {
             nums[i] = Integer.parseInt(input[i]);
         }
 
+        // Read rotation value
         int k = sc.nextInt();
 
-        rotateArray(nums, k);
+        // Call rotate function
+        rotateArray(nums, k);  // calling file name only 
+
         sc.close();
     }
 }
---
+++ StringBuilder/TakingInput016.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+
+public class TakingInput016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder(sc.nextInt());
+        System.out.println(sb);
+    }
+}
